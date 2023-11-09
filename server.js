3c269fa0c0ff51a985a023641057c6b6e9013223
Zaki_1052 <zakiralibhai@gmail.com>
2023-11-09 04:24:31 -0800

    Key Change

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.post('/message', async (req, res) => {
   
     // Define the data payload with system message and additional parameters
     const data = {
-      model: "gpt-3.5-turbo-1106",
+      model: "gpt-3.5-turbo-0613",
       messages: [
         { role: "system", content: "You're a helpful assistant." },
         { role: "user", content: user_message }
