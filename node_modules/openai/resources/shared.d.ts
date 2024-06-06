bb61b0097b8aad49881b4bda95d6ebafd9bbafe2
Zaki-1052 <zakiralibhai@gmail.com>
2024-06-05 19:58:41 -0700

    npm

--- node_modules/openai/resources/shared.d.ts
+++ node_modules/openai/resources/shared.d.ts
@@ -17,8 +17,8 @@ export interface FunctionDefinition {
     description?: string;
     /**
      * The parameters the functions accepts, described as a JSON Schema object. See the
-     * [guide](https://platform.openai.com/docs/guides/text-generation/function-calling)
-     * for examples, and the
+     * [guide](https://platform.openai.com/docs/guides/function-calling) for examples,
+     * and the
      * [JSON Schema reference](https://json-schema.org/understanding-json-schema/) for
      * documentation about the format.
      *
@@ -28,8 +28,8 @@ export interface FunctionDefinition {
 }
 /**
  * The parameters the functions accepts, described as a JSON Schema object. See the
- * [guide](https://platform.openai.com/docs/guides/text-generation/function-calling)
- * for examples, and the
+ * [guide](https://platform.openai.com/docs/guides/function-calling) for examples,
+ * and the
  * [JSON Schema reference](https://json-schema.org/understanding-json-schema/) for
  * documentation about the format.
  *
